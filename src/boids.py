fdc11873a68305d4f2d47127f8d76e497dcefe45
Tim Herrin <teffluvium@gmail.com>
2022-05-19 01:24:15 -0700

    Updates Boid
    * Data members store a dx, dy velocity vector instead of speed and direction
    * Adds angle and speed @property methods
    @ Adds move method

--- src/boids.py
+++ src/boids.py
@@ -1,23 +1,51 @@
 from dataclasses import dataclass
 from typing import Tuple
 
+from math import cos, sin, radians, degrees, atan2, sqrt
+import numpy as np
+
+rng = np.random.default_rng()
 
 @dataclass
 class Boid:
     """A single boid in the flock"""
     position: Tuple[int, int] = (0, 0)
-    speed: float = 5
-    direction: float = 0
+    velocity: Tuple[int, int] = (0, 0)
     color: Tuple[int, int, int] = (255, 0, 0)
     size: int = 1
+
+    @property
+    def angle(self):
+        """Calculate the angle of the boid"""
+        return np.rad2deg(np.arctan2(self.velocity[1], self.velocity[0]))
+
+    @property
+    def speed(self):
+        """Calculate the speed of the boid"""
+        return sqrt(
+            sum(v ** 2 for v in self.velocity)
+        )
+
+    def move(self):
+        """Move the boid"""
+        # Update the position relative to the velocity
+        self.position = (
+            self.position[0] + self.velocity[0],
+            self.position[1] + self.velocity[1],
+        )
 
     def __str__(self):
         return ", ".join(
             [
                 f"Boid({self.position=}",
-                f"{self.direction=:.2f}",
-                f"{self.speed=:.2f}",
+                f"{self.velocity=}",
                 f"{self.color=}",
                 f"{self.size=})",
             ]
         )
+
+if __name__     == "__main__":
+    a = Boid(position=(0, 0), velocity=(1, 1), color=(0, 0, 0), size=1)
+    b = Boid(position=(0, 0), velocity=(1, 1), color=(0, 0, 0), size=1)
+    
+    print(a == b)
