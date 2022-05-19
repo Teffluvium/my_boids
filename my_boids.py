f02cce496eb9a5fecb39447896167874f6c0784c
Tim Herrin <teffluvium@gmail.com>
2022-05-19 01:28:55 -0700

    Implements basic boid flocking algorithms
    * Adds fly_to_center_of_mass
    * Adds avoid_other_boids
    * Adds match_velocity
    * Adds clamp_speed

--- my_boids.py
+++ my_boids.py
@@ -1,27 +1,27 @@
 import math
+from typing import overload
 
 import numpy as np
 import pygame as pg
-import pygame.gfxdraw
 
 from boids import Boid
 
 # Constants
 WINSIZE = [640, 480]
 WINCENTER = [320, 240]
-NUMBOIDS = 3
+NUMBOIDS = 100
+BOIDSIZE = 5
+BOIDMAXSPEED = 5
 
 rng = np.random.default_rng()
 
-
-def pol2cart(rho, phi):
-    x = rho * math.cos(math.radians(phi))
-    y = rho * math.sin(math.radians(phi))
-    return (x, y)
+# Create global variable for the boids
+boids = []
 
 
 def init_boids(num_boids: int) -> list:
     """Initialize the list of boids
+    Assigns random positions, velocities, and colors
 
     Args:
         num_boids (int): number of boids to initialize
@@ -30,42 +30,140 @@ def init_boids(num_boids: int) -> list:
         list: list of boids
     """
     boids_list = []
-    for n in range(num_boids):
+    for _ in range(num_boids):
         boid = Boid(
             position=rng.integers(0, high=WINSIZE, size=2).tolist(),
-            direction=rng.uniform(0, 360),
-            speed=5,
-            color=rng.integers(10, 255, 3).tolist(),
-            size=5,
+            velocity=rng.integers(0, high=WINSIZE, size=2).tolist(),
+            color=rng.integers(20, 255, 3).tolist(),
+            size=BOIDSIZE,
         )
         boids_list.append(boid)
+
     return boids_list
 
 
-def draw_boids(screen, boids, color=None):
+def draw_boid(screen, boid, color=None):
+    """Draw the boid on the screen"""
     # Draw the boids
-    for boid in boids:
-        # Override color if provided
-        if not color:
-            color = boid.color
-            
-        pg.draw.circle(
-            screen,
-            color=color,
-            center=boid.position,
-            radius=boid.size,
+    override_color = bool(color)
+
+    # Override color if provided
+    if not override_color:
+        color = boid.color
+
+    # Draw the boid
+    pg.draw.circle(
+        screen,
+        color=color,
+        center=boid.position,
+        radius=boid.size,
+    )
+    # Draw the velociy vector
+    pg.draw.line(
+        screen,
+        color=color,
+        start_pos=boid.position,
+        end_pos=(
+            boid.position[0] + boid.velocity[0],
+            boid.position[1] + boid.velocity[1],
+        ),
+    )
+
+
+def move_boid(boid):
+    """Move the boid according to its velocity"""
+    boid.move()
+
+    # Make boids wrap around the screen
+    boid.position = (
+        boid.position[0] % WINSIZE[0],
+        boid.position[1] % WINSIZE[1],
+    )
+
+
+def fly_to_center_of_mass(boid):
+    """Move the boid towards the perceived center of mass of the flock"""
+    global boids
+
+    update_factor = 0.01  # How much to update the boid's velocity
+
+    # Calculate the center of mass
+    sum_of_x = sum(b.position[0] for b in boids)
+    sum_of_x -= boid.position[0]
+    sum_of_y = sum(b.position[1] for b in boids)
+    sum_of_y -= boid.position[1]
+    center_of_mass = (sum_of_x / (NUMBOIDS - 1), sum_of_y / (NUMBOIDS - 1))
+
+    delta_x = (center_of_mass[0] - boid.position[0]) * update_factor
+    delta_y = (center_of_mass[1] - boid.position[1]) * update_factor
+
+    # Update the boid's velocity
+    boid.velocity[0] += delta_x
+    boid.velocity[1] += delta_y
+
+
+def avoid_other_boids(boid):
+    """Avoid other boids that are too close"""
+    global boids
+
+    min_distance = 20
+    avoid_factor = 0.5
+
+    delta_x = 0
+    delta_y = 0
+    for other_boid in boids:
+        # Skip checking the boid itself
+        if other_boid is boid:
+            continue
+
+        # Calculate the distance between the boids
+        distance = math.sqrt(
+            (boid.position[0] - other_boid.position[0]) ** 2
+            + (boid.position[1] - other_boid.position[1]) ** 2
         )
-        dir_x, dir_y = pol2cart(boid.speed + boid.size, boid.direction)
-        pg.draw.line(
-            screen,
-            color=color,
-            start_pos=boid.position,
-            end_pos=(boid.position[0] + dir_x, boid.position[1] + dir_y),
-        )
+
+        # If the distance is less than the minimum, apply the avoidance
+        if distance < min_distance:
+            # Calculate the vector to the other boid
+            delta_x += boid.position[0] - other_boid.position[0]
+            delta_y += boid.position[1] - other_boid.position[1]
+
+        # Apply the vector to the boid
+        boid.velocity[0] += delta_x * avoid_factor
+        boid.velocity[1] += delta_y * avoid_factor
+
+
+def match_velocity(boid):
+    """Match the velocity of the boid with the velocity of the flock"""
+    global boids
+
+    matching_factor = 0.01
+
+    # Calculate the average velocity of the flock
+    sum_of_x = sum(b.velocity[0] for b in boids)
+    sum_of_x -= boid.velocity[0]
+    sum_of_y = sum(b.velocity[1] for b in boids)
+    sum_of_y -= boid.velocity[1]
+    average_velocity = (sum_of_x / (NUMBOIDS - 1), sum_of_y / (NUMBOIDS - 1))
+
+    # Update the boid's velocity
+    boid.velocity[0] += average_velocity[0] * matching_factor
+    boid.velocity[1] += average_velocity[1] * matching_factor
+
+
+def clamp_speed(boid):
+    speed = boid.speed
+    # Apply a speed limit
+    if speed > BOIDMAXSPEED:
+        boid.velocity = [
+            boid.velocity[0] * BOIDMAXSPEED / speed,
+            boid.velocity[1] * BOIDMAXSPEED / speed,
+        ]
 
 
 def main():
     # Initialize the boids
+    global boids
     boids = init_boids(NUMBOIDS)
     # [print(b) for b in boids]
 
@@ -80,9 +178,21 @@ def main():
     # Main game loop
     done = False
     while not done:
-        draw_boids(screen, boids, color="black")
-        # move_boids(boids)
-        draw_boids(screen, boids)
+        for boid in boids:
+            # Erease current boid
+            draw_boid(screen, boid, color="black")
+
+            # Apply movement rules
+            fly_to_center_of_mass(boid)
+            avoid_other_boids(boid)
+            match_velocity(boid)
+            clamp_speed(boid)
+
+            move_boid(boid)
+
+            # Draw the updated boid
+            draw_boid(screen, boid)
+
         pg.display.update()
         for event in pg.event.get():
             # Close window or hit escape
