232f3001f00c29415191923bf26855650247b5d9
Tim Herrin <teffluvium@gmail.com>
2022-05-26 17:40:06 -0700

    Moves all functionality into single file
    * Should be refactored later into separate class / function files

--- boids_sprite_sim.py
+++ boids_sprite_sim.py
@@ -10,11 +10,11 @@ Explanation video: http://youtu.be/O4Y5KrNgP_c
 """
 
 import configparser
+from enum import Enum, auto
+from typing import Tuple
 
 import numpy as np
 import pygame as pg
-
-from boids.movement import BoundaryType, move_boid
 
 # --- Global constants ---
 BLACK = (0, 0, 0)
@@ -26,6 +26,15 @@ SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 500
 
 rng = np.random.default_rng()
+
+# Boid Constants
+SIZE = 10
+MAX_SPEED = 3
+COHESION_FACTOR = 0.001
+SEPARATION = 20
+AVOID_FACTOR = 0.01
+ALIGNMENT_FACTOR = 0.01
+VISUAL_RANGE = 100
 
 
 # --- Classes ---
@@ -63,8 +72,8 @@ class Boid(pg.sprite.Sprite):
         self.image = pg.Surface([width, height])
 
         # Fill the surface with the the background color and set it to be transparent
-        self.image.fill(BLACK)
-        self.image.set_colorkey(BLACK)
+        self.image.fill(pg.Color("black"))
+        self.image.set_colorkey(pg.Color("black"))
 
         # Draw the boid onto the surface
         pg.draw.ellipse(self.image, color, [0, 0, width, height])
@@ -74,11 +83,11 @@ class Boid(pg.sprite.Sprite):
 
         self.rect.center = self.pos
 
-    def reset_pos(self):
+    def reset_pos(self, window_size: tuple = (SCREEN_WIDTH, SCREEN_HEIGHT)):
         """Called when the boid 'falls off' the screen."""
-        boundary_type = BoundaryType.BOUNCE
-
-        move_boid(self, boundary_type, [SCREEN_WIDTH, SCREEN_HEIGHT])
+        # TODO: move this function out of Boid class
+        boundary_type = BoundaryType.WRAP
+        move_boid(self, boundary_type, window_size=window_size)
 
     def update(self):
         """Called each frame. Updates the position of the boid."""
@@ -90,6 +99,156 @@ class Boid(pg.sprite.Sprite):
 
         # Update the rectangle position
         self.rect.center = self.pos
+
+    def avoid_other_boids(
+        self,
+        boids: list,
+        separation: float = SEPARATION,
+        avoid_factor: float = AVOID_FACTOR,
+    ):
+        """Avoid other boids that are too close"""
+        delta = pg.Vector2(0, 0)
+        for other_boid in boids:
+            # Skip checking the boid itself
+            if other_boid is self:
+                continue
+
+            # Calculate the distance between the boids
+            distance = self.pos.distance_to(other_boid.pos)
+
+            # If the distance is less than the minimum, apply the avoidance
+            if distance < separation:
+                # Calculate the vector to the other boid
+                delta += self.pos - other_boid.pos
+
+        # Apply the vector to the boid
+        self.vel += delta * avoid_factor
+
+    def match_velocity(
+        self,
+        boids: list,
+        alignment_factor: float = ALIGNMENT_FACTOR,
+        visual_range: float = VISUAL_RANGE,
+    ):
+        """Match the velocity of the boid with the velocity of the flock"""
+        num_boids = 0
+
+        # Calculate the average velocity of the flock, as long as they are
+        # within the visual range
+        sum_velocity = pg.Vector2(0, 0)
+        for boid in boids:
+            if boid.pos.distance_to(self.pos) < visual_range:
+                sum_velocity += boid.vel
+                num_boids += 1
+
+        # There should at least 2 boids, yourself and another boid
+        if num_boids >= 2:
+            # Subract the boid's own velocity contribution
+            sum_velocity -= self.vel
+
+            # Calculate the average velocity of other boids
+            average_velocity = sum_velocity / (num_boids - 1)
+
+            # Update the boid's velocity
+            self.vel += (average_velocity - self.vel) * alignment_factor
+
+    def speed_limit(self, max_speed: float = MAX_SPEED):
+        """Limit the speed of the boid"""
+        speed = self.vel.magnitude()
+        # Apply a speed limit
+        if speed > max_speed:
+            self.vel.scale_to_length(max_speed)
+
+
+class BoundaryType(Enum):
+    """Enum for boid behavior when they hit the boundary"""
+
+    BOUNCE = auto()
+    WRAP = auto()
+
+
+def move_boid(
+    boid: Boid,
+    boundary_type: BoundaryType,
+    window_size: Tuple[int, int],
+    margin: int = 30,
+    turn_factor: float = 1,
+) -> None:
+    """Move a boid and keep it within the windows according to the boundary type.
+
+    Args:
+        boid (Boid): Its a Boid
+        boundary_type (BoundaryType): What to do when the boid hits the edge
+        window_size (Tuple[int, int]): Window size in pixels
+    """
+
+    # Select the boundary type and adjust the boid's position and/or velocity
+    if boundary_type == BoundaryType.WRAP:
+        wrap_around_screen(
+            boid,
+            window_size,
+        )
+    elif boundary_type == BoundaryType.BOUNCE:
+        keep_within_bounds(
+            boid,
+            window_size,
+            margin=margin,
+            turn_factor=turn_factor,
+        )
+
+
+def wrap_around_screen(boid: Boid, window_size: Tuple[int, int]) -> None:
+    """Wrap boid around to opposite side of the window.
+
+    Note: This function compares the postion of the boid to the window size
+        and adjusts the position, leaving the velocity unchanged.
+
+    Args:
+        boid (Boid): Its a Boid
+        window_size (Tuple[int, int]): Window size in pixels
+    """
+    boid.pos.update(
+        boid.pos[0] % window_size[0],
+        boid.pos[1] % window_size[1],
+    )
+
+
+def keep_within_bounds(
+    boid: Boid,
+    window_size: Tuple[int, int],
+    margin: int = 30,
+    turn_factor: float = 1,
+) -> None:
+    """Adjust the boid's velocity to keep it within the window.
+
+    Note: This function compares the postion of the boid to the window size
+        and adjusts the velocity, leaving the position unchanged.
+
+    Args:
+        boid (Boid): Its a Boid
+        window_size (Tuple[int, int]): Window size in pixels
+        margin (int, optional): Buffer of pixels from the edges of the
+            window. Defaults to 30.
+        turn_factor (float, optional): Adjust the velocity by this factor.
+            Defaults to 1.
+    """
+
+    def adjust_vel(pos, vel, window_size) -> float:
+        """Adjust velocity component to keep boid within bounds"""
+        if pos < margin:
+            vel += turn_factor
+        elif pos > window_size - margin:
+            vel -= turn_factor
+
+        return vel
+
+    # Calculate the new velocity
+    vel = [0.0, 0.0]
+    vel[0] = adjust_vel(boid.pos[0], boid.vel[0], window_size[0])
+    vel[1] = adjust_vel(boid.pos[1], boid.vel[1], window_size[1])
+
+    # Update the boid's velocity
+    boid.vel.update(vel)
 
 
 class Player(pg.sprite.Sprite):
@@ -164,6 +323,11 @@ class Game(object):
         if not self.game_over:
             # Move all the sprites
             self.all_sprites_list.update()
+
+            # TODO: Apply screen edge behavior and movement rules here
+            # Incluedes: cohesion, avoid_other_boids, match_velocity, setting boundary_type, and move_boid
+            for boid in self.block_list:
+                pass
 
             # See if the player boid has collided with anything.
             blocks_hit_list = pg.sprite.spritecollide(
